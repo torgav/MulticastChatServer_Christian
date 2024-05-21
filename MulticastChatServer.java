5bc6a1a9c07de5a5659af963d58c66932e330ab7
torgav <christian.torresgaviria@elev.ga.ntig.se>
2024-05-21 12:58:05 +0200

    Skrev ner allt kod

---
+++ MulticastChatServer.java
@@ -0,0 +1,39 @@
+import java.net.*;
+
+public class MulticastChatServer {
+
+    public static void main(String args[])
+            throws Exception {
+
+        // Default port number we are going to use
+        int portnumber = 5000;
+        if (args.length >= 1) {
+            portnumber = Integer.parseInt(args[0]);
+        }
+
+        // Create a MulticastSocket
+        MulticastSocket serverMulticastSocket =
+                new MulticastSocket(portnumber);
+        System.out.println("MulticastSocket is created at port " + portnumber);
+
+        // Determine the IP address of a host, given the host name
+        InetAddress group =
+                InetAddress.getByName("225.4.5.6");
+
+        // getByName- returns IP address of given host
+        serverMulticastSocket.joinGroup(group);
+        System.out.println("joinGroup method is called...");
+        boolean infinite = true;
+
+        // Continually receives data and prints them
+        while (infinite) {
+            byte buf[] = new byte[1024];
+            DatagramPacket data =
+                    new DatagramPacket(buf, buf.length);
+            serverMulticastSocket.receive(data);
+            String msg =
+                    new String(data.getData()).trim();
+            System.out.println("Message received from client = " + msg);
+        }
+        serverMulticastSocket.close();
+    }
